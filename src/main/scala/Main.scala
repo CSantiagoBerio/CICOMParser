f29cdde45ad1350e939e7ad68c91a4b8e641b634
Christian Santiago <christian.santiago23@upr.edu>
2018-10-08 11:05:36 -0400

    Minor bugs fixed

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,14 +6,19 @@ object Main extends CICOM {
     val source = Source.fromFile("src\\main\\scala\\testprogram").getLines().mkString
 
     parse(Exp, source) match {
-      case Success(matched, _) => println(matched)
+      case Success(matched, _) => println("\nParsing Successful, Program Syntactically Correct...\n" + matched)
       case Failure(msg, _) => println(s"Failure: $msg")
       case Error(msg, _) => println(s"Error: $msg")
     }
+    println()
 
     val parser = new CICOM
     val result = parser.parseAll(parser.Exp, source)
-    println()
+    if (result.successful) {
+      println("Parsing Successful, Program Syntactically Correct...")
+    } else {
+      println("Parsing failed, Program Syntactically Incorrect")
+    }
     println(result.toString)
 
   }
